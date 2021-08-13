c8ae534b0e0922c5e0495478f16d37554438bf44
dingziqi <dingziqi_xtu@sina.com>
2021-08-13 17:18:31 +0800

    build(global): 支持tsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
+      <Seo title="首页" />
       {/* <Bio /> */}
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
